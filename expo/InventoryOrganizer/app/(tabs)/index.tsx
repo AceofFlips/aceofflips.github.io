e232ed26653acf5587f424e935c45d264ab5be9b
Ace of Flips/Madeleine <174635729+AceofFlips@users.noreply.github.com>
2026-01-29 11:49:36 -0500

    Make an API call
    +Made an API call. :)
    
    DO NOT MERGE INTO MAIN. THIS IS FOR REFERENCE ONLY

--- expo/InventoryOrganizer/app/(tabs)/index.tsx
+++ expo/InventoryOrganizer/app/(tabs)/index.tsx
@@ -1,10 +1,15 @@
-import { Text, View, StyleSheet } from "react-native";
+import {Text, View, StyleSheet, FlatList, ActivityIndicator} from "react-native";
 import { Link } from 'expo-router';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
-
+import {useEffect, useState} from "react";
+import {useFetch} from "@/services/api";
 
 export default function Index() {
+    const{data,error,loading, refetch} = useFetch("https://dummyjson.com/users");
+
+    if (loading) return <ActivityIndicator size={"large"} color={"red"}/>
+    if (error) return <Text>Error: {error}</Text>
   return (
     <ThemedView style={styles.container}>
       <ThemedText type = "title">
@@ -15,6 +20,14 @@ export default function Index() {
                 Go to About screen
             </ThemedText>
         </Link>
+        <FlatList data={data} renderItem={({item}) =>(
+             <View style={styles.wrapper}>
+                 <Text>{item?.username}</Text>
+                 <Text>{item?.birthDate}</Text>
+             </View>
+            )}>
+
+        </FlatList>
     </ThemedView>
   );
 }
@@ -22,7 +35,12 @@ export default function Index() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        padding: 14,
         justifyContent: "center",
         alignItems: "center",
     },
+    wrapper: {
+        padding:14,
+        marginVertical: 10,
+    }
 })
